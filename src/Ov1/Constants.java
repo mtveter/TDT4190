5a028ca03b91bdc6409d50c3c0012449cf97ddb9
petteas <petter.astrup@gmail.com>
2014-01-29 15:16:40 +0100

    Endret Constants

--- src/Ov1/Constants.java
+++ src/Ov1/Constants.java
@@ -1,19 +1,6 @@
 package Ov1;
 
 public interface Constants {
-    /** The breadth and width of the board */
-    public static final int BOARD_SIZE=8;
-    /** The number of marks in a row required to win */
-    public static final int WINNER_LENGTH = 5;
-    /** The width of the window */
-    public static final int WIN_WIDTH = 380;
-    /** The height of the window */
-    public static final int WIN_HEIGHT = 400;
-    /** The width of a square in the board */
-    public static final int SQUARE_WIDTH = 30;
-    /** The height of a square in the board */
-    public static final int SQUARE_HEIGHT = 30;
-    /** IP address used for testing */
     public static final String IP = "127.0.0.1";
     /** Port number used for testing */
     public static final String PORT = "3040"; //
