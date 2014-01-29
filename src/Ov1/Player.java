d2abc8a55a26acc123b89d6c20d28c6b68e53ced
petteas <petter.astrup@gmail.com>
2014-01-29 14:59:57 +0100

    La til shit

---
+++ src/Ov1/Player.java
@@ -0,0 +1,5 @@
+package Ov1;
+
+public class Player {
+
+}
--- src/Ov1/RMIInterface.java
+++ src/Ov1/RMIInterface.java
@@ -3,5 +3,5 @@ package Ov1;
 import java.rmi.Remote;
 
 public interface RMIInterface extends Remote{
-
+	public boolean checkwin(char x);
 }
